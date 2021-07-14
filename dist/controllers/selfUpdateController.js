0d225a365227ff30569c474d2e7e2d55da5a8e78
Jelle Siderius <jelle@hypershop.nl>
2021-07-14 21:30:57 +0200

    0.1.4

--- dist/controllers/selfUpdateController.js
+++ dist/controllers/selfUpdateController.js
@@ -5,21 +5,16 @@ const tslib_1 = require("tslib");
 const download_git_repo_1 = tslib_1.__importDefault(require("download-git-repo"));
 const get_installed_path_1 = require("get-installed-path");
 // @ts-ignore
-const package_json_1 = tslib_1.__importDefault(require("../../package.json"));
 class SelfUpdateController {
     constructor() {
         this.executeStart = (serviceName) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            // Get NPM path to this module
             let npmPath = '';
-            let oldVersion = package_json_1.default.version;
-            let newVersion = '';
             yield get_installed_path_1.getInstalledPath('mage-db-sync').then((path) => {
                 npmPath = path;
             });
-            yield download_git_repo_1.default('jellesiderius/mage-db-sync', npmPath, function (err) {
-                newVersion = package_json_1.default.version;
+            yield download_git_repo_1.default('jellesiderius/mage-db-sync#master', npmPath, function (err) {
+                console.log('Updated to newest version of mage-db-sync');
             });
-            yield console.log(`Updated from ${oldVersion} to ${newVersion}`);
             return true;
         });
     }
--- src/controllers/selfUpdateController.ts
+++ src/controllers/selfUpdateController.ts
@@ -2,24 +2,18 @@
 import download from 'download-git-repo'
 import {getInstalledPath} from 'get-installed-path'
 // @ts-ignore
-import packageJson from '../../package.json'
 
 class SelfUpdateController {
     executeStart = async (serviceName: string | undefined): Promise<boolean> => {
-        // Get NPM path to this module
         let npmPath = '';
-        let oldVersion = packageJson.version;
-        let newVersion = '';
 
         await getInstalledPath('mage-db-sync').then((path) => {
             npmPath = path;
         });
 
-        await download('jellesiderius/mage-db-sync', npmPath, function (err: any) {
-            newVersion = packageJson.version;
+        await download('jellesiderius/mage-db-sync#master', npmPath, function (err: any) {
+            console.log('Updated to newest version of mage-db-sync');
         });
-
-        await console.log(`Updated from ${oldVersion} to ${newVersion}`);
 
         return true;
     }
