d0511cf9ac18963754abc13a2e1b40d6126534fd
hypershopjelle <jelle@hypershop.nl>
2021-07-18 18:06:16 +0200

    - wip

--- dist/controllers/testController.js
+++ dist/controllers/testController.js
@@ -9,16 +9,21 @@ class TestController extends mainController_1.default {
     constructor() {
         super(...arguments);
         this.executeStart = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
+            // Ask all the questions to the user
             yield this.askQuestions();
+            // Confure the task list
             yield this.configureList();
+            // Run the task list
             yield this.list.run();
         });
         // Ask questions to user
         this.askQuestions = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
+            // Clear the console
             console_1.clearConsole();
             // Ask question about database type
             let databaseTypeQuestion = yield new databaseTypeQuestion_1.default();
             yield databaseTypeQuestion.configure(this.config);
+            // Clear the console
             console_1.clearConsole();
         });
         // Configure task list
--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -6,19 +6,26 @@ import MainController from "./mainController";
 
 class TestController extends MainController {    
     executeStart = async (): Promise<void> => {
+        // Ask all the questions to the user
         await this.askQuestions();
+        
+        // Confure the task list
         await this.configureList();
+        
+        // Run the task list
         await this.list.run();
     }
 
     // Ask questions to user
     askQuestions = async () => {
+        // Clear the console
         clearConsole();
-        
+
         // Ask question about database type
         let databaseTypeQuestion = await new DatabaseTypeQuestion();
         await databaseTypeQuestion.configure(this.config);
-
+        
+        // Clear the console
         clearConsole();
     }
 
