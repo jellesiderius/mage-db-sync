2fdb0c9ae2863c98917d2cb929f7fdcad7411ad0
Jelle Siderius <jelle@hypershop.nl>
2021-08-02 12:42:49 +0200

    Put back files

--- src/tasks/magentoConfigureTask.ts
+++ src/tasks/magentoConfigureTask.ts
@@ -132,7 +132,7 @@ class MagentoConfigureTask {
                     for (const [key, value] of Object.entries(allWebsites)) {
                         // @ts-ignore
                         let code = value.code;
-                        await localhostMagentoRootExec(`magerun2 customer:create ${configFile.magentoBackend.adminEmailAddress} ${configFile.magentoBackend.adminPassword} Firstname Lastname ${code} > /dev/null 2>&1`, config);
+                        await localhostMagentoRootExec(`magerun2 customer:create ${configFile.magentoBackend.adminEmailAddress} ${configFile.magentoBackend.adminPassword} Firstname Lastname ${code}`, config);
                     }
                 }
             }
