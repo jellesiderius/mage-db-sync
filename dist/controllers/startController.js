880ad019682a0b0ba48435a706474e6354d70917
Jelle Siderius <jelle@hypershop.nl>
2023-08-02 11:33:28 +0200

    Release 1.1.7

--- dist/controllers/startController.js
+++ dist/controllers/startController.js
@@ -84,6 +84,7 @@ class StartController extends mainController_1.default {
         });
         // Configure task list
         this.prepareTasks = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
+            (0, console_1.info)(' === Running all download & configuration tasks, this can take a couple of minutes, get yourself some coffee or tea === ✨ \n');
             if (this.config.settings.syncDatabases == 'yes') {
                 // Sync databases tasks
                 // Build up check list
